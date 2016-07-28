37c395d4f9ad0f2ee2d710b9052ab82d51382f3b
saurabhvats <saurabh.vats@in.hanusoftware.com>
2016-07-28 13:10:05 +0530

    Add project files

---
+++ TestWebRole/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TestWebRole.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
